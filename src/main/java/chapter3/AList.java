49daf2805a392216377d266f90e8a5407ff3ff92
Arun Gajaraj <arun.gajaraj@aspiresys.com>
2017-05-25 09:53:05 +0530

    new files

--- src/main/java/chapter3/AList.java
+++ src/main/java/chapter3/AList.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.*;
 
 public class AList {
 	
@@ -17,6 +18,48 @@ public class AList {
 
 		System.out.println(list);
 		System.out.println(list.get(0));
+		
+		String[] array = {"Arun", "Divakar", "Ananth", "NoOne"};
+
+		List<String> arrayList = Arrays.asList(array);
+
+		System.out.println("ArrayList");
+		System.out.println(arrayList);
+		System.out.println("===================");	
+		printArray(array);
+
+		// arrayList.remove("NoOne");	//Unsupported
+		arrayList.set(3,"People");
+		// arrayList.add("Ghost");		//Unsupported
+
+		System.out.println("ArrayList");
+		System.out.println(arrayList);
+		System.out.println("===================");	
+		printArray(array);
+
+		array[2] = "Everyone";
+
+		System.out.println("ArrayList");
+		System.out.println(arrayList);
+		System.out.println("===================");	
+		printArray(array);
+
+		/***********************/
+
+		String[] arr = (String[]) list.toArray();
+		System.out.println("List : " + list);
+		System.out.println("Array : ");
+		printArray(array);
+
+		list.set(1,"Updated");
+		printArray(array);
+
+	}
+
+	public static void printArray(String[] array){
+		for (String string : array) {
+			System.out.println(string);			
+		}
 	}
 	
 }
